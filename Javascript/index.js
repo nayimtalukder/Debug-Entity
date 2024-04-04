76046a61784d6ebac6ef7818a2025e5263142c5c
nayimtalukder <dr.nayimtalukder@gmail.com>
2024-04-05 01:59:00 +0600

    Eightteen

---
+++ Javascript/index.js
@@ -0,0 +1,16 @@
+
+
+const x = document.getElementById("menu");
+
+const menu = ()=>{
+
+    if(x.style.display == "none"){
+        x.style.display = "block"
+    }
+    else{
+
+        x.style.display = "none"
+    }
+
+}
+
